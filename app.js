7c242aa405ff28e5535c497f683fd294420ff79a
Jack Shewchuk <jack@shew.net>
2022-11-08 15:38:23 -0500

    Testing

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const nodeEnv = process.env.NODE_ENV;
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/images'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+let food = [{
+  id: 1,
+  title: 'sushi',
+  place: 'worchester'
+}];
+
+
+app.get('/api/food', (req, res) => {
+  res.send(food);
 })
+
+
 
 // app.put('/user', (req, res) => {
 //     res.send('Got a PUT request at /user')
