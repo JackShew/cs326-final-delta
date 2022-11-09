3a5806c067b758d984d98dcfcb45712a1724a8c6
Jack Shewchuk <jack@shew.net>
2022-11-09 15:54:20 -0500

    added POST

--- app.js
+++ app.js
@@ -1,22 +1,65 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const nodeEnv = process.env.NODE_ENV;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/images'))
 
+app.post('/dish', (req, res) => {
+  const dish = req.body;
 
-let food = [{
-  id: 1,
-  title: 'sushi',
-  place: 'worchester'
-}];
+  // Output the book to the console for debugging
+  console.log(dish);
+  worchester.push(dish);
 
+  res.send(worchester);
+});
 
-app.get('/api/food', (req, res) => {
-  res.send(food);
+app.get('/hi', (req, res) => {
+  res.send('Hello World, from express');
+});
+
+app.get('/api/worchester/', (req, res) => {
+  res.send(worchester);
 })
 
+let worchester = [
+  {
+    title: 'sushi',
+    description: 'Loris Impsum',
+    image: '/public/images/sushi.png',
+    upvotes: 100,
+    comments: 4
+  },
+  {
+    title: 'stirfry',
+    description: 'Loris Impsum',
+    image: '/public/images/stirFry.webp',
+    upvotes: 19,
+    comments: 1
+  }
+];
+
+
+
+
+// app.post('/book', (req, res) => {
+//   const book = req.body;
+
+//   // Output the book to the console for debugging
+//   console.log(book);
+//   books.push(book);
+
+//   res.send('Book is added to the database');
+// });
 
 
 // app.put('/user', (req, res) => {
