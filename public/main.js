8cc2dfff4258cdf681b5eda98b1bc94166483141
Jayden <jnambu@umass.edu>
2022-11-09 14:19:09 -0500

    functional increment/decrement for franklin Sushi

--- public/main.js
+++ public/main.js
@@ -1,1 +1,50 @@
-document.getElementById('red').classList.add("hidden");// console.log(document.getElementById('red').innerText);
+// document.getElementById('red').classList.add("hidden");// console.log(document.getElementById('red').innerText);
+const dishes = {};
+dishes["Franklin/Sushi/Score"] = {incremented: false, decremented: false};
+
+document.getElementById("Franklin/Sushi/Score/increment").addEventListener('click', function(){
+    increment("Franklin/Sushi/Score")
+});
+document.getElementById("Franklin/Sushi/Score/decrement").addEventListener('click', function(){
+    decrement("Franklin/Sushi/Score")
+});
+function increment(id){
+    if(!dishes[id]["incremented"]){
+        let score = document.getElementById(id).innerText;
+        score = parseInt(score);
+        score += 1;
+        if(dishes[id]["decremented"]){
+            score+=1;
+        }
+        document.getElementById(id).innerText = score.toString();
+        dishes[id]["incremented"] = true;
+        dishes[id]["decremented"] = false;
+    }else{
+        let score = document.getElementById(id).innerText;
+        score = parseInt(score);
+        score -= 1;
+        document.getElementById(id).innerText = score.toString();
+        dishes[id]["incremented"] = false;
+        dishes[id]["decremented"] = false;
+    }
+}
+function decrement(id){
+    if(!dishes[id]["decremented"]){
+        let score = document.getElementById(id).innerText;
+        score = parseInt(score);
+        score -= 1;
+        if(dishes[id]["incremented"]){ 
+            score -= 1;
+        }
+        document.getElementById(id).innerText = score.toString();
+        dishes[id]["decremented"] = true;
+        dishes[id]["incremented"] = false;
+    }else{
+        let score = document.getElementById(id).innerText;
+        score = parseInt(score);
+        score += 1;
+        document.getElementById(id).innerText = score.toString();
+        dishes[id]["incremented"] = false;
+        dishes[id]["decremented"] = false;
+    }
+}
