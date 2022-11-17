3f38ac234e6ace0e1d6d9f933cded8c1d695a550
Jack Shewchuk <jack@shew.net>
2022-11-17 17:51:02 -0500

    Merge conflicts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(cors());
 const {MongoClient} = require("mongodb");
 const uri = process.env.MONGODB_URI;
-
+console.log(uri);
 // require('dotenv').config();
 // var mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/GrubGauge');
@@ -87,6 +87,31 @@ let worchester = [
   }
 ];
 
+
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
 
 
 
