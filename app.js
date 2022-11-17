14d9965b233c15a37c85269720b721ba61e81c00
Jayden <jnambu@umass.edu>
2022-11-17 15:15:40 -0500

    mongodb test datbase implemented

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const nodeEnv = process.env.NODE_ENV;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
+const {MongoClient} = require("mongodb");
+const uri = process.env.MONGODB_URI;
 
-require('dotenv').config();
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/GrubGauge');
+// require('dotenv').config();
+// var mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/GrubGauge');
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,6 +28,40 @@ app.post('/dish', (req, res) => {
   res.send(worchester);
 });
 
+app.get('/mongo', async function(req,res) {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const collection = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { genres: "Comedy", poster: { $exists: true } };
+    const cursor = await collection.aggregate([
+      { $match: query },
+      { $sample: { size: 1 } },
+      { $project: 
+        {
+          title: 1,
+          fullplot: 1,
+          poster: 1
+        }
+      }
+    ]);
+
+    const movie = await cursor.next();
+
+    return res.json(movie);
+  } catch(err) {
+    console.log(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+})
 app.get('/hi', (req, res) => {
   res.send('Hello World, from express');
 });
@@ -72,3 +108,5 @@ let worchester = [
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// mongodb+srv://JaydenNambu:<GGShoko03>@grubgauge-east.kusf5zy.mongodb.net/GrubGaugeData?retryWrites=true&w=majority
