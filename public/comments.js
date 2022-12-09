9dee05c223f4fc81fc084dcdcac18776dfde3c29
Jayden Nambu <jnambu@umass.edu>
2022-12-09 12:33:47 -0500

    comment score now comes from data

--- public/comments.js
+++ public/comments.js
@@ -12,7 +12,8 @@ window.addEventListener("load", async function() {
     const test = {
         "commenter":"george",
         "date" : date,
-        "text": "cool"
+        "text": "cool",
+        "score": 56
     }
     renderComment(test)
 });
@@ -191,7 +192,7 @@ function renderComment(commentData){
             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
         </svg>
     </button>
-    <div class = "comment-score"><b>34</b></div>
+    <div class = "comment-score"><b>${commentData["score"]}</b></div>
     <button class="btn btn-default">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
